32773b00b0c489d4d760759bd34be978a78b77ef
Joshy14 <133443388+ayaank37@users.noreply.github.com>
2025-03-04 18:55:48 -0500

    app

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import ipinfo
+import folium
+key = '2a2f20187c8678'
+ip = input("Enter IPadress for research: ")
+handler = ipinfo.getHandler(key)
+details = handler.getDetails(ip)
+map =  folium.Map(location=[details.latitude, details.longitude])
+map
+print('City: '+details.city)
+print('Coordinates: '+details.loc)
