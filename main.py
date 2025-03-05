928e6ad314beba42570904225e4e375f98a03c5a
Joshua Sambol <97353253+Joshy14@users.noreply.github.com>
2025-03-05 19:57:28 +0000

    updates and fixed pull user IP

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import ipinfo
 import requests
 #https://whatismyv6.com/home.cgi
 #import folium
-import socket
+
 
 key = '2a2f20187c8678'#4 ipinfo API
 
-local_ip = socket.gethostbyname(socket.gethostname())
-print("Your Local IP:", local_ip)
 
 
-userIp = requests.get("https://ipinfo.io/ip").text.strip()
-print("Your Public IP:", userI, "(may not work)")
+userData = requests.get("https://ipinfo.io/json").json()
+ipv6 = requests.get("https://api64.ipify.org").text.strip()
 
-ip = input("Enter IPadress for research: ")
+print("Here is your public IPv4 data: ")
+print(f"IP: {userData['ip']}, City: {userData['city']}, Region: {userData['region']}, Country: {userData['country']}")
+print("**Your Data may be going through a cloud server**")
+
+
+ip = input("Enter costom IP address for research: ")
 handler = ipinfo.getHandler(key)
 details = handler.getDetails(ip)
 #map =  folium.Map(location=[details.latitude, details.longitude])
 print('City: '+details.city)
+print('Country:'+details.country)
+print ('Region:'+details.region)
 print('Coordinates: '+details.loc)
