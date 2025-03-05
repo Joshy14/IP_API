b1cb6b2147a913ff05bb77503aff53167fea9e57
Joshua Sambol <97353253+Joshy14@users.noreply.github.com>
2025-03-05 18:00:50 +0000

    User IP

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import ipinfo
-import folium
-key = '2a2f20187c8678'
+import requests
+#https://whatismyv6.com/home.cgi
+#import folium
+import socket
+
+key = '2a2f20187c8678'#4 ipinfo API
+
+local_ip = socket.gethostbyname(socket.gethostname())
+print("Your Local IP:", local_ip)
+
+
+userIp = requests.get("https://ipinfo.io/ip").text.strip()
+print("Your Public IP:", userI, "(may not work)")
+
 ip = input("Enter IPadress for research: ")
 handler = ipinfo.getHandler(key)
 details = handler.getDetails(ip)
-map =  folium.Map(location=[details.latitude, details.longitude])
+#map =  folium.Map(location=[details.latitude, details.longitude])
 print('City: '+details.city)
 print('Coordinates: '+details.loc)
